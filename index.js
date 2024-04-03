76db0c67ea45344798bb54a7ce7cbc22cba7ec84
ak14092003 <arpitdav4444@gmail.com>
2024-04-04 00:41:42 +0530

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+// // $("h1").addClass("big-title margin-50");
+// $("h1").text("bye");
+
+
+// // $(".btn-1").text("dont");
+// $("button").html("<em>Hey</em>")
+// $("h1").click(function(){
+//    $("h1").css("color", "purple"); 
+// })
+  $("button").click(function(){
+         $("h1").slideUp().slideDown().animate({opacity: 0.5});
+  });
+// $(document).keydown(function(event){
+//     $("h1").html(event.key)
+// })
+
+// $("h1").on("mouseover", function(){
+//     $("h1").css("color", "red");
+// })
